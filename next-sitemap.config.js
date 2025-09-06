1389dccb65f336f71fd15717ae8cca3875003720
GusTZer0 <gucarioyt@gmail.com>
2025-09-06 00:01:27 -0300

    Delete next-sitemap.config.js

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://syndromemaincomputer.app',
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-  changefreq: 'monthly',
-  priority: 0.7,
-}
